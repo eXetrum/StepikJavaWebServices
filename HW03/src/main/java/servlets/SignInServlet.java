6c1967a6a180a9bde37a4ddbec5f9c3a3f799dbd
eXetrum <shishimorovmaksim@gmail.com>
2024-03-19 15:11:02 +0200

    skeleton code from HW02

---
+++ HW03/src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+public class SignInServlet extends HttpServlet {
+    private final AccountService accountService;
+    public SignInServlet(AccountService accountService) { this.accountService = accountService; }
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if(login == null || password == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+
+        if(profile == null || !profile.getPassword().equals(password)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Unauthorized");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        accountService.addSession(request.getSession().getId(), profile);
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("Authorized: " + profile.getLogin());
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+}
